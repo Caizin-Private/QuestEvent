dce31cc80b374c03e162038fb01ec70bfc974a3b
Rohan Lande <rohan.lande@caizin.com>
2025-12-26 12:58:46 +0530

    refac: updated wallet entity

--- questevent-backend/src/main/java/com/questevent/entity/User.java
+++
@@ -1,4 +0,0 @@
-package com.questevent.entity;
-
-public class User {
-}
--- questevent-backend/src/main/java/com/questevent/entity/Wallet.java
+++ questevent-backend/src/main/java/com/questevent/entity/Wallet.java
@@ -1,21 +1,30 @@
 package com.questevent.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
+@Data
 @Entity
 @Table(name = "wallets")
 public class Wallet {
 
     @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
+    @Column(name="coins")
     private int coins;
+
+    @Column(name="gems")
     private int gems;
 
 }
