a405f9e75d0ddc969479f6d83ebe1acc0d227864
Abhinash-caizin <Abhinash.samal@caizin.com>
2025-12-29 10:23:29 +0530

    Addd logic of submission service

--- questevent-backend/src/main/java/com/questevent/service/SubmissionService.java
+++ questevent-backend/src/main/java/com/questevent/service/SubmissionService.java
@@ -1,4 +1,10 @@
 package com.questevent.service;
 
 public interface SubmissionService {
+
+    void submitActivity(
+            Long activityId,
+            Long userId,
+            String submissionUrl
+    );
 }
