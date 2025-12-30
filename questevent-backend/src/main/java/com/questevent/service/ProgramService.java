28d2e12bbb596861d5fb31c372d115e5992b359e
nikhil-caizin <nikhil.negi@caizin.com>
2025-12-30 12:13:33 +0530

    feat: add program wallet entry in program wallet table when program created

--- questevent-backend/src/main/java/com/questevent/service/ProgramService.java
+++ questevent-backend/src/main/java/com/questevent/service/ProgramService.java
@@ -3,6 +3,7 @@ package com.questevent.service;
 import com.questevent.entity.Program;
 import com.questevent.entity.User;
 import com.questevent.repository.ProgramRepository;
+import com.questevent.repository.ProgramWalletRepository;
 import com.questevent.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,17 +15,23 @@ public class ProgramService {
 
     private final ProgramRepository programRepository;
     private final UserRepository userRepository;
+    private final ProgramWalletRepository programWalletRepository;
+    private final ProgramWalletService programWalletService;
 
     @Autowired
-    public ProgramService(ProgramRepository programRepository, UserRepository userRepository) {
+    public ProgramService(ProgramRepository programRepository, UserRepository userRepository, ProgramWalletRepository programWalletRepository, ProgramWalletService programWalletService) {
         this.programRepository = programRepository;
         this.userRepository = userRepository;
+        this.programWalletRepository = programWalletRepository;
+        this.programWalletService = programWalletService;
     }
 
     public Program createProgram(Long userId, Program program) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
         program.setUser(user);
+        programWalletService.createWallet(userId, program.getProgramId());
+
         return programRepository.save(program);
     }
 }
