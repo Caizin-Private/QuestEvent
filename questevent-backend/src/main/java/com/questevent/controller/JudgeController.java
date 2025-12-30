a1468941cb56f5d46ceb820bad53aabacde240ff
Abhinash-caizin <Abhinash.samal@caizin.com>
2025-12-30 10:08:42 +0530

    created DTO for the submission controller

--- questevent-backend/src/main/java/com/questevent/controller/JudgeController.java
+++ questevent-backend/src/main/java/com/questevent/controller/JudgeController.java
@@ -1,6 +1,6 @@
 package com.questevent.controller;
 
-import com.questevent.entity.ActivitySubmission;
+import com.questevent.dto.ActivitySubmissionDto;
 import com.questevent.service.JudgeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,23 +19,37 @@ public class JudgeController {
      * Fetch all submissions for a given activity
      * Used by Judge Dashboard
      */
+
     @GetMapping("/submissions/{activityId}")
-    public ResponseEntity<List<ActivitySubmission>> getSubmissionsForActivity(
+    public ResponseEntity<List<ActivitySubmissionDto>> getSubmissionsForActivity(
             @PathVariable Long activityId
     ) {
-        return ResponseEntity.ok(
+        List<ActivitySubmissionDto> response =
                 judgeService.getSubmissionsForActivity(activityId)
-        );
+                        .stream()
+                        .map(submission -> new ActivitySubmissionDto(
+                                submission.getSubmissionId(),
+                                submission.getActivityRegistration().getActivity().getActivityId(),
+                                submission.getActivityRegistration().getUser().getUserId(),
+                                submission.getSubmissionUrl(),
+                                submission.getAwardedGems(),
+                                submission.getSubmittedAt(),
+                                submission.getReviewedAt()
+                        ))
+                        .toList();
+
+        return ResponseEntity.ok(response);
     }
 
     /**
      * Review a submission and award gems
      */
+
     @PostMapping("/review/{submissionId}")
     public ResponseEntity<String> reviewSubmission(
             @PathVariable Long submissionId,
             @RequestParam Integer awardedGems
-    ) {
+    ){
         judgeService.reviewSubmission(submissionId, awardedGems);
         return ResponseEntity.ok("Submission reviewed successfully");
     }
