c8385c360da83be6cdb04d6bbbd77d5fa23082df
caizin-yashnikam <yash.nikam@caizin.com>
2025-12-31 12:11:08 +0530

    created the programregistrationDTO

---
+++ questevent-backend/src/main/java/com/questevent/dto/ProgramRegistrationDTO.java
@@ -0,0 +1,22 @@
+package com.questevent.dto;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgramRegistrationDTO {
+    private Long programRegistrationId;
+    private Long programId;
+    private String programTitle;
+    private Long userId;
+    private String userName;
+    private LocalDateTime registeredAt;
+}
+
+
+
