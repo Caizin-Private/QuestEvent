ab8e79cefd9d1c157654329862376c353dcff2fd
caizin-sumitKumar <sumit.kumar@caizin.com>
2025-12-31 12:20:48 +0530

    created activity registration response dto

---
+++ questevent-backend/src/main/java/com/questevent/dto/ActivityRegistrationResponseDTO.java
@@ -0,0 +1,21 @@
+package com.questevent.dto;
+
+import com.questevent.enums.CompletionStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class ActivityRegistrationResponseDTO {
+    private Long activityRegistrationId;
+    private Long activityId;
+    private String activityName;
+    private Long userId;
+    private String userName;
+    private String userEmail;
+    private CompletionStatus completionStatus;
+    private Integer rewardGems;
+    private String message;
+}
