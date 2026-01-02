12eae97db7c8535f166f41c919135c051b3eb3de
nikhil-caizin <nikhil.negi@caizin.com>
2026-01-02 11:31:18 +0530

    minor fix

--- questevent-backend/src/test/java/ProgramWalletTransactionServiceImplTest.java
+++ questevent-backend/src/test/java/ProgramWalletTransactionServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class ProgramWalletTransactionServiceImplTest {
+public class ProgramWalletTransactionServiceImplTest {
 
     @Mock
     private ProgramWalletRepository programWalletRepository;
