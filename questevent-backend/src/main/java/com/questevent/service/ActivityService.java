0d064357c4c1f832b976f7b1ea44d24739b766bb
Abhinash-caizin <Abhinash.samal@caizin.com>
2026-01-07 10:48:34 +0530

    reliability fix

--- questevent-backend/src/main/java/com/questevent/service/ActivityService.java
+++ questevent-backend/src/main/java/com/questevent/service/ActivityService.java
@@ -15,11 +15,17 @@ import java.util.List;
 @Service
 public class ActivityService {
 
-    @Autowired
-    private ActivityRepository activityRepository;
 
-    @Autowired
-    private ProgramRepository programRepository;
+    private final ActivityRepository activityRepository;
+    private final ProgramRepository programRepository;
+
+    public ActivityService(
+            ActivityRepository activityRepository,
+            ProgramRepository programRepository
+    ) {
+        this.activityRepository = activityRepository;
+        this.programRepository = programRepository;
+    }
 
     public Activity createActivity(Long programId, ActivityRequestDTO dto) {
         Program program = programRepository.findById(programId)
