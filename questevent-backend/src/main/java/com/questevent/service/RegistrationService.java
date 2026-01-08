b70c4c0304ca8e0ec736785281f4a39e0b6bd7fe
Rohan Lande <rohan.lande@caizin.com>
2026-01-08 09:40:44 +0530

    refact: removed registration controller and services

--- questevent-backend/src/main/java/com/questevent/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.questevent.service;
-
-public interface RegistrationService {
-
-    void registerForProgram(Long programId, Long userId);
-
-    void registerForActivity(Long activityId, Long userId);
-}
-
