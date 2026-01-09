b02462325799ed63a1276d12a4f00d091993ebbe
Rohan Lande <rohan.lande@caizin.com>
2026-01-09 12:57:58 +0530

    refactor: add userid auto increment

--- questevent-backend/src/main/java/com/questevent/entity/User.java
+++ questevent-backend/src/main/java/com/questevent/entity/User.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false, updatable = false)
     private Long userId;
 
